164ab30906de32146551faa72c5e6aad5407917c
llukoj@gmail.com <llukoj@gmail.com@3ee95134-bef6-849b-e8d7-a1357358da0d>
2012-03-15 18:19:00 +0000

    byrjaður að vinna á * operator búinn með texta margföldun bætti við falli til að gera það endilega láttu mig vita hvort þetta sé í lagi.

--- StringCraft/Core/DataTypes/Stringel.cs
+++ StringCraft/Core/DataTypes/Stringel.cs
@@ -11,6 +11,12 @@ namespace StringCraft
 		public readonly char TextColor;
 		public readonly char BackColor;
 		
+		/**
+		 * List of all possable outcomes for * operator
+		 * */
+		
+		//Outchar 
+		
 		public Stringel(char text, char textColor, char backColor)
 		{
 			Text = text;
@@ -21,6 +27,7 @@ namespace StringCraft
 		{
 			ConsoleWriter.Write(new Rectangle(position, Vector2.ONE, false), new CharInfo[]{GetBuffer()});
 		}
+		
 		public CharInfo GetBuffer()
 		{
 			return new CharInfo
@@ -29,6 +36,7 @@ namespace StringCraft
 				Char = new CharUnion{ UnicodeChar = Text }
 			};
 		}
+		
 		public void Print()
 		{
 			System.Console.BackgroundColor = BackColor.ToColor();
@@ -47,7 +55,82 @@ namespace StringCraft
 		}
 		public static Stringel operator *(Stringel c1, Stringel c2)
 		{
-			return c2;
+			char textColor;
+			char backColor;
+			char text;
+			Stringel c3 = new Stringel();
+			
+			//Base case check if c2 is transparent
+			if(c2.TextColor == '?')
+			{	
+				if(c2.BackColor == '?')
+				{
+					return c1;
+				}
+			}
+			//Finds the back color
+			if(c2.BackColor == '?')
+			{
+				backColor=c1.BackColor;
+			}
+			else 
+			{
+			}
+			
+			//Finds the text Color
+			
+			//Finds the text
+			text=c3.TextMulti(c1.Text,c2.Text);
+			
+			return c1;
+		}
+		
+		// Function that takes in 2 char values and puts them together if possiable and returns the result
+		// If not it returns the firsChar
+		public char TextMulti(char firstChar, char SecondChar)
+		{
+			int y=0;
+			int x=0;
+			char[,] textMulti=
+			{{'?','/','\\','|','_','-','P','A','E','F','(',')','V','D','c','.'},
+			{'/','?','x','?','?','?','?','?','?','?','?','?','?','?','?','?'},
+			{'\\','x','?','?','?','?','R','?','?','?','?','?','?','?','?','?'},
+			{'|','?','?','?','L','+','?','?','?','?','?','D','?','?','d','i'},
+			{'_','?','?','L','?','?','?','?','?','E','?','?','?','?','?','?'},
+			{'-','?','?','+','?','?','?','?','?','?','?','?','?','Ð','?','?'},
+			{'P','?','R','?','?','?','?','?','?','?','?','?','?','?','?','?'},
+			{'A','?','?','?','?','?','?','?','Æ','?','?','?','?','?','?','?'},
+			{'E','?','?','?','?','?','?','Æ','?','?','?','?','?','?','?','?'},
+			{'F','?','?','?','E','?','?','?','?','?','?','?','?','?','?','?'},
+			{'(','?','?','?','?','?','?','?','?','?','?','O','?','?','?','?'},
+			{')','?','?','D','?','?','?','?','?','?','O','?','?','?','?','?'},
+			{'V','?','?','?','?','?','?','?','?','?','?','?','W','?','?','?'},
+			{'D','?','?','?','?','Ð','?','?','?','?','?','?','?','?','?','?'},
+			{'c','?','?','d','?','?','?','?','?','?','?','?','?','?','?','?'},
+			{'.','?','?','i','?','?','?','?','?','?','?','?','?','?','?',':'}};
+			
+			for(int i=1; i<16; i++)
+			{
+				if(firstChar == textMulti[0,i])
+				{
+					y=i;
+				}
+				
+				if(firstChar == textMulti[i,0])
+				{
+					x=i;
+				}
+			}
+			
+			if((x!=0)&&(y!=0))
+			{
+				if(textMulti[x,y]!='?')
+				{
+					return textMulti[x,y];
+				}
+			}
+			
+			return firstChar;
 		}
 	}
 }
